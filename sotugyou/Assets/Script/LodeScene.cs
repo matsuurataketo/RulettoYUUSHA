b31f574ca99403326ec931737e545cc48ee45d01
matsuurataketo <90751819+matsuurataketo@users.noreply.github.com>
2024-11-15 22:27:01 +0900

    画像の追加とタイトルのボタン操作の変更

--- sotugyou/Assets/Script/LodeScene.cs
+++ sotugyou/Assets/Script/LodeScene.cs
@@ -11,4 +11,14 @@ public class LodeScene : MonoBehaviour
     {
         SceneManager.LoadScene(SceneName);
     }
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        // エディタで動作を確認する場合
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        // 実行ファイルで終了する場合
+        Application.Quit();
+#endif
+    }
 }
