a2bcf9d8ea2b3d4602d8514e6ea79d6ed3fd3683
matsuurataketo <90751819+matsuurataketo@users.noreply.github.com>
2024-12-14 12:30:53 +0900

    全部のシーン切り替えにフェードをつけていきなり切り替わるように見えなくしました

--- sotugyou/Assets/Script/LodeScene.cs
+++ sotugyou/Assets/Script/LodeScene.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class LodeScene : MonoBehaviour
 {
     [SerializeField, Header("移動するシーンの名前")] private string SceneName;
+    public Image fadeImage; // フェード用のImage
+    public float fadeDuration = 1f; // フェード時間
+    public bool fadeIn = true;
+
     // Start is called before the first frame update
+    private void Start()
+    {
+        if (fadeIn == true)
+            StartCoroutine(FadeIn());
+    }
     public void OnClickGameStart()
     {
-        SceneManager.LoadScene(SceneName);
+        StartCoroutine(FadeOut(SceneName));
     }
     public void ExitGame()
     {
@@ -24,5 +34,36 @@ public class LodeScene : MonoBehaviour
     public void PushButton()
     {
         Debug.Log("何かおこります");
+    }
+    private IEnumerator FadeIn()
+    {
+        fadeImage.gameObject.SetActive(true);
+        Color color = fadeImage.color;
+
+        for (float t = 0; t < fadeDuration; t += Time.deltaTime)
+        {
+            color.a = 1f - t / fadeDuration;
+            fadeImage.color = color;
+            yield return null;
+        }
+
+        color.a = 0f;
+        fadeImage.color = color;
+        fadeImage.gameObject.SetActive(false);
+    }
+    private IEnumerator FadeOut(string sceneName)
+    {
+        fadeImage.gameObject.SetActive(true);
+        Color color = fadeImage.color;
+        for (float t = 0; t < fadeDuration; t += Time.deltaTime)
+        {
+            color.a = t / fadeDuration;
+            fadeImage.color = color;
+            yield return null;
+        }
+        color.a = 1f;
+        fadeImage.color = color;
+
+        SceneManager.LoadScene(sceneName);
     }
 }
